efbf3503360bd8c0e099bbe1b6ef7f38dc526230
matheusprado1 <matheusprado1096@gmail.com>
2025-04-15 17:26:10 -0300

    feat: update controllers

--- src/server/controllers/cities/GetAll.ts
+++ src/server/controllers/cities/GetAll.ts
@@ -19,7 +19,13 @@ export const getAllValidation = validation((getSchema) => ({
 }));
 
 export const getAll = async (req: Request<{}, {}, object, IQueryProps>, res: Response) => {
-  console.log(req.query);
+  res.setHeader('access-control-expose-headers', 'x-total-count');
+  res.setHeader('x-total-count', 1);
 
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado');
+  res.status(StatusCodes.OK).json([
+    {
+      id: 1,
+      name: 'Araguari',
+    }
+  ]);
 };
--- src/server/controllers/cities/UpdateById.ts
+++ src/server/controllers/cities/UpdateById.ts
@@ -9,7 +9,7 @@ interface IParamProps {
 };
 interface IBodyProps {
   name: string;
-}
+};
 
 export const updateByIdValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(yup.object().shape({
@@ -21,8 +21,11 @@ export const updateByIdValidation = validation((getSchema) => ({
 }));
 
 export const updateById = async (req: Request<IParamProps>, res: Response) => {
-  console.log(req.params);
-  console.log(req.body);
+  if (Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    errors: {
+      default: 'Registro não encontrado'
+    }
+  });
 
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado');
+  res.status(StatusCodes.NO_CONTENT).send();
 };
