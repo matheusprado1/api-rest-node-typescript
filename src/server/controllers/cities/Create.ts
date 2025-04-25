7328b5fd7264db19fc9244a85fd858069315a82b
matheusprado1 <matheusprado1096@gmail.com>
2025-04-25 10:49:03 -0300

    feat: added city provider

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { StatusCodes } from 'http-status-codes';
 
+import { CitiesProvider } from '../../database/providers/cities';
 import { validation } from '../../shared/middlewares';
 import { ICity } from '../../database/models';
 
@@ -9,12 +10,20 @@ interface IBodyProps extends Omit<ICity, 'id'> { };
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(yup.object().shape({
-    name: yup.string().required().min(3),
+    name: yup.string().required().min(3).max(150),
   })),
 }));
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
-  console.log(req.body);
+  const result = await CitiesProvider.create(req.body);
 
-  res.status(StatusCodes.CREATED).json(1);
+  if (result instanceof Error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      }
+    });
+  }
+
+  res.status(StatusCodes.CREATED).json(result);
 };
